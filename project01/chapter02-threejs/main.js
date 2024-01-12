cf329d62f284ae577017e3abb91fec9774215167
yumin-jung <jungym887@naver.com>
2024-01-12 22:29:30 +0900

    feat: Three.js - Geometry 실습

--- project01/chapter02-threejs/main.js
+++ project01/chapter02-threejs/main.js
@@ -1,5 +1,11 @@
 import "./style.css";
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.shadowMap.enabled = true;
+document.body.appendChild(renderer.domElement);
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -13,14 +19,125 @@ const camera = new THREE.PerspectiveCamera(
 camera.position.y = 1;
 camera.position.z = 5;
 
+// 직사광선
+const directionalLight = new THREE.DirectionalLight(0xffffff, 5);
+directionalLight.castShadow = true;
+directionalLight.position.set(3, 4, 5);
+directionalLight.lookAt(0, 0, 0);
+scene.add(directionalLight);
+
+const floorGeometry = new THREE.PlaneGeometry(20, 20);
+const floorMaterial = new THREE.MeshStandardMaterial({ color: 0xbbbbbb });
+const floor = new THREE.Mesh(floorGeometry, floorMaterial);
+floor.rotation.x = -Math.PI / 2;
+floor.receiveShadow = true;
+floor.castShadow = true;
+scene.add(floor);
+
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const material = new THREE.MeshStandardMaterial({ color: 0xff0000 });
 const mesh = new THREE.Mesh(geometry, material);
+mesh.castShadow = true;
+mesh.position.y = 0.5;
 scene.add(mesh);
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+const capsuleGeometry = new THREE.CapsuleGeometry(1, 2, 20, 30);
+const capsuleMaterial = new THREE.MeshStandardMaterial({ color: 0xffff00 });
+const capsuleMesh = new THREE.Mesh(capsuleGeometry, capsuleMaterial);
+capsuleMesh.position.set(3, 1.75, 0);
+capsuleMesh.castShadow = true;
+capsuleMesh.receiveShadow = true;
+scene.add(capsuleMesh);
+
+const cylinderGeometry = new THREE.CylinderGeometry(1, 1, 2);
+const cylinderMaterial = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
+const cylinderMesh = new THREE.Mesh(cylinderGeometry, cylinderMaterial);
+cylinderMesh.position.set(-3, 1, 0);
+cylinderMesh.castShadow = true;
+cylinderMesh.receiveShadow = true;
+scene.add(cylinderMesh);
+
+const torusGeometry = new THREE.TorusGeometry(0.5, 0.1, 16, 100);
+const torusMaterial = new THREE.MeshStandardMaterial({ color: 0x0000ff });
+const torusMesh = new THREE.Mesh(torusGeometry, torusMaterial);
+torusMesh.position.set(0, 0.5, 1);
+torusMesh.castShadow = true;
+torusMesh.receiveShadow = true;
+scene.add(torusMesh);
+
+const starShape = new THREE.Shape();
+starShape.moveTo(0, 1);
+starShape.lineTo(0.2, 0.2);
+starShape.lineTo(1, 0.2);
+starShape.lineTo(0.4, -0.1);
+starShape.lineTo(0.6, -1);
+starShape.lineTo(0, -0.5);
+starShape.lineTo(-0.6, -1);
+starShape.lineTo(-0.4, -0.1);
+starShape.lineTo(-1, 0.2);
+starShape.lineTo(-0.2, 0.2);
+const shapeGeometry = new THREE.ShapeGeometry(starShape);
+const shapeMaterial = new THREE.MeshStandardMaterial({ color: 0xff00ff });
+const shapeMesh = new THREE.Mesh(shapeGeometry, shapeMaterial);
+shapeMesh.position.set(0, 1, 2);
+scene.add(shapeMesh);
+
+const extrudeSettings = {
+    steps: 1,
+    depth: 0.1,
+    bevelEnabled: true,
+    bevelThickness: 0.1,
+    bevelSize: 0.3,
+    bevelSegments: 100,
+};
+
+const extrudeGeometry = new THREE.ExtrudeGeometry(starShape, extrudeSettings);
+const extrudeMaterial = new THREE.MeshStandardMaterial({ color: 0x0ddaaf });
+const extrudeMesh = new THREE.Mesh(extrudeGeometry, extrudeMaterial);
+extrudeMesh.position.set(2, 1.3, 2);
+extrudeMesh.castShadow = true;
+extrudeMesh.receiveShadow = true;
+scene.add(extrudeMesh);
+
+const sphereGeometry = new THREE.SphereGeometry(1, 32, 32);
+const sphereMaterial = new THREE.MeshStandardMaterial({ color: 0x98daaf });
+const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+sphere.position.set(-2, 1, -3);
+sphere.castShadow = true;
+sphere.receiveShadow = true;
+scene.add(sphere);
+
+const numPoints = 1000;
+const positions = new Float32Array(numPoints * 3);
+
+for (let i = 0; i < numPoints; i += 1) {
+    const x = (Math.random() - 0.5) * 1;
+    const y = (Math.random() - 0.5) * 1;
+    const z = (Math.random() - 0.5) * 1;
+
+    positions[i * 3] = x;
+    positions[i * 3 + 1] = y;
+    positions[i * 3 + 2] = z;
+}
+
+// GPU를 더 많이 사용하여 효율적임
+const bufferGeometry = new THREE.BufferGeometry();
+bufferGeometry.setAttribute(
+    "position",
+    new THREE.BufferAttribute(positions, 3)
+);
+
+const pointsMaterial = new THREE.PointsMaterial({
+    color: 0xffff00,
+    size: 0.05
+});
+
+const point = new THREE.Points(bufferGeometry, pointsMaterial);
+point.position.set(0, 0, -5);
+scene.add(point);
+
+const orbitControls = new OrbitControls(camera, renderer.domElement);
+orbitControls.update();
 
 window.addEventListener('resize', () => {
     renderer.setSize(window.innerWidth, window.innerHeight);
@@ -28,4 +145,10 @@ window.addEventListener('resize', () => {
     camera.updateProjectionMatrix();
     renderer.render(scene, camera);
 })
-renderer.render(scene, camera);
+
+const render = () => {
+    renderer.render(scene, camera);
+    requestAnimationFrame(render);
+}
+
+render();
